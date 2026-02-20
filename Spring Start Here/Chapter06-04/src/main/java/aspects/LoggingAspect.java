dd935bc48afdba601c89c077333c01b5acf3e90e
parkgunwook <ss944161269441@gmail.com>
2026-02-20 18:06:20 +0900

    Spring Start Here: Chapter 6

---
+++ Spring Start Here/Chapter06-04/src/main/java/aspects/LoggingAspect.java
@@ -0,0 +1,34 @@
+package aspects;
+
+import model.Comment;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Aspect
+@Order(2)
+public class LoggingAspect {
+    private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+
+    @Around("@annotation(ToLog)")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] arguments = joinPoint.getArgs();
+
+        logger.info("Method " + methodName + " with parameters " + Arrays.asList(arguments) + " will execute");
+
+        Comment comment = new Comment();
+        comment.setText("Some other text!");
+        Object[] newArgyments = {comment};
+
+        Object returnedByMethod = joinPoint.proceed(newArgyments);
+
+        logger.info("Method executed and returned " + returnedByMethod);
+
+        return "FAILED";
+    }
+}
