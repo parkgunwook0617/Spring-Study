cabc2ae6d1e94960de1ccdd333b9247021d159b1
parkgunwook <ss944161269441@gmail.com>
2026-02-21 16:16:24 +0900

    Spring Start Here: Chapter 7

---
+++ Spring Start Here/Chapter07-01/src/main/java/performance/chapter0701/Chapter0701Application.java
@@ -0,0 +1,13 @@
+package performance.chapter0701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter0701Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter0701Application.class, args);
+    }
+
+}
---
+++ Spring Start Here/Chapter07-01/src/test/java/performance/chapter0701/Chapter0701ApplicationTests.java
@@ -0,0 +1,13 @@
+package performance.chapter0701;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter0701ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
