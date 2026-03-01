0428ea31a5a26b29171d6b58a04282f3b5d491c9
parkgunwook <ss944161269441@gmail.com>
2026-03-01 12:40:50 +0900

    Spring Start Here: Chapter 15

---
+++ Spring Start Here/Chapter09-01/src/test/java/performance/chapter0901/LoginControllerUnitTests.java
@@ -0,0 +1,48 @@
+package performance.chapter0901;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
+import performance.chapter0901.controller.LoginController;
+import performance.chapter0901.model.LoginProcessor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class LoginControllerUnitTests {
+    @Mock
+    private Model model;
+
+    @Mock
+    private LoginProcessor loginProcessor;
+
+    @InjectMocks
+    private LoginController loginController;
+
+    @Test
+    public void loginPostLoginSucceedsTest() {
+        given(loginProcessor.login()).willReturn(true);
+
+        String result = loginController.loginPost("username", "password", model);
+
+        assertEquals("login.html", result);
+
+        verify(model).addAttribute("message", "You are now logged in.");
+    }
+
+    @Test
+    public void loginPostLoginFailsTest() {
+        given(loginProcessor.login()).willReturn(false);
+
+        String result = loginController.loginPost("username", "password", model);
+
+        assertEquals("login.html", result);
+
+        verify(model).addAttribute("message", "Login failed!");
+    }
+}
